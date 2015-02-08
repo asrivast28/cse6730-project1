5c89988cfcfd34dc65eb0320b1273bb6893333bb
Ankit Srivastava <asrivast@gatech.edu>
2015-02-07 19:43:42 -0500

    Adding SimulationEngine.hpp

---
+++ code/SimulationEngine.hpp
@@ -0,0 +1,62 @@
+#include "Event.hpp"
+
+#include <queue>
+
+class SimulationEngine {
+public:
+  SimulationEngine();
+
+  void
+  run();
+
+  void
+  schedule(
+    double,
+    const EventData&,
+    void (*) (const EventData&)
+  );
+
+  double
+  currentTime() const;
+
+private:
+  std::priority_queue<Event> m_fel;
+  double m_simtime;
+}; // class SimulationEngine
+
+SimulationEngine::SimulationEngine(
+) : m_fel(), m_simtime(0)
+{
+}
+
+void
+SimulationEngine::run(
+)
+{
+  while (m_fel.size() > 0) {
+    const Event event(m_fel.top());
+    m_fel.pop();
+    m_simtime = event.timestamp();
+    event.callback();
+  }
+}
+
+// schedule the event at time-stamp, and provide a callback to its handler
+void
+SimulationEngine::schedule(
+  double timestamp,
+  const EventData& eventData,
+  EventType eventType,
+  void (*callback) (const EventData&)
+)
+{
+  m_fel.emplace(timestamp, callback, eventData);
+}
+
+// returns the current simulation time
+double
+SimulationEngine::currentTime(
+) const
+{
+	return m_simtime;
+}
