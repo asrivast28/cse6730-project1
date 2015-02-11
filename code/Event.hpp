cf5b52d60db88a8dda2d88cc9aa1f3b199134a6a
Ankit Srivastava <asrivast@gatech.edu>
2015-02-11 15:42:32 -0500

    Moved TrafficEvent specific things to TrafficEvent.hpp

--- code/Event.hpp
+++ code/Event.hpp
@@ -3,23 +3,6 @@
 
 #include <memory>
 
-typedef enum { Tenth = 10, Eleventh, Twelfth, Thirteenth, Fourteenth, Fifteenth } Street;
-
-/**
- * @brief Structure for storing all the information for a vehicle.
- */
-typedef struct {
-  unsigned id; // ID of the vehicle.
-  Street origin; // Originating street of the vehicle.
-  Street destination; // Destination street of the vehicle.
-  unsigned entryTime; // Time at which the vehicle entered the area of interest.
-  unsigned exitTime; // Time at which the vehicle exited from the area of interest.
-  double startWaiting; // Time at which the vehicle started waiting at the previous intersection.
-  double endWaiting; // Time at which the vehicle passed the previous intersection.
-  double totalWaiting; // Total waiting time for the vehicle;
-	double currentPosition; // Indicate the current position of the vehicle.
-} Vehicle;
-
 /**
  * @brief Class containing simulation event attributes.
  */
--- code/TrafficEvent.hpp
+++ code/TrafficEvent.hpp
@@ -4,6 +4,24 @@
 
 #include "Event.hpp"
 
+typedef enum { Tenth = 10, Eleventh, Twelfth, Thirteenth, Fourteenth, Fifteenth } Street;
+
+/**
+ * @brief Structure for storing all the information for a vehicle.
+ */
+typedef struct {
+  unsigned id; // ID of the vehicle.
+  Street origin; // Originating street of the vehicle.
+  Street destination; // Destination street of the vehicle.
+  double entryTime; // Time at which the vehicle entered the area of interest.
+  double exitTime; // Time at which the vehicle exited from the area of interest.
+  double startWaiting; // Time at which the vehicle started waiting at the previous intersection.
+  double endWaiting; // Time at which the vehicle passed the previous intersection.
+  double totalWaiting; // Total waiting time for the vehicle;
+	double currentPosition; // Indicate the current position of the vehicle.
+} Vehicle;
+
+
 class TrafficEvent : public Event {
 public:
   TrafficEvent(
